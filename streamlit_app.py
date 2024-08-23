25c76857e0dd3ed1f5e3ea67c5544ea80f6d94a3
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-08-23 16:06:57 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,10 +31,11 @@ def pagina_principal():
    # Read
    st.write(st.session_state)
     # Verifica se a chave 'mensagens' existe no st.session_state
-   if 'mensagens' not in st.session_state:
-        st.session_state.mensagens = 'a'  # Inicializa 'mensagens' como uma lista vazia
-
-   mensagens = st.session_state['mensagens']  # Acessa a lista de mensagens
+   #if 'mensagens' not in st.session_state:
+   #     st.session_state.mensagens = 'a'  # Inicializa 'mensagens' como uma lista vazia
+   if 'key' not in st.session_state:
+    st.session_state['key'] = 'value'
+   #mensagens = st.session_state['mensagens']  # Acessa a lista de mensagens
 
    st.header('Deco GPT', divider=True)
 
