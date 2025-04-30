5b5bef0d84a37a26782c1afdab83748e7d83da7f
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-04-30 14:01:32 -0300

    Create pandasai.py

---
+++ pages/pandasai.py
@@ -0,0 +1,101 @@
+import streamlit as st
+from openai import OpenAI
+import os
+from groq import Groq
+
+# Configurações da página
+st.set_page_config(
+    page_title="DecoGPT",
+    page_icon="🤖",
+    #layout="wide",
+    #initial_sidebar_state='collapsed'
+) 
+
+
+# Setup OpenAI
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+
+
+client = OpenAI(
+   api_key=OPENAI_API_KEY,
+ )
+
+# Criar função para retornar a mensagem do modelo
+def retorna_resposta_modelo(mensagens,
+                            openai_key = OPENAI_API_KEY,
+                            modelo = 'gpt-4o-mini-2024-07-18',
+                            temperatura=0,
+                            stream=False):
+  response = client.chat.completions.create(
+    model = modelo,
+    messages = mensagens,
+    temperature = temperatura,
+    stream = stream
+)
+  return response
+
+# Setup Groq
+GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
+
+clientgroq = Groq(
+   api_key=GROQ_API_KEY,
+ )
+
+# Criar função para retornar a mensagem do modelo
+def retorna_resposta_modelo_groq(mensagens,
+                            api_key = GROQ_API_KEY,
+                            modelo = 'llama3-8b-8192',
+                            temperatura=0,
+                            stream=True):
+  response = clientgroq.chat.completions.create(
+    model = modelo,
+    messages = mensagens,
+    temperature = temperatura,
+    stream = stream
+)
+  return response
+
+import streamlit as st
+
+# Função para armazenar a página principal
+def pagina_principal():
+    # Verifica se a chave 'mensagens' existe no st.session_state
+   if 'mensagens' not in st.session_state:
+        st.session_state.mensagens = []  # Inicializa 'mensagens' como uma lista vazia
+
+   mensagens = st.session_state['mensagens']  # Acessa a lista de mensagens
+
+   st.header('Deco GPT', divider=True)
+
+   for mensagem in mensagens:
+      chat = st.chat_message(mensagem['role'])
+      chat.markdown(mensagem['content'])
+
+   prompt = st.chat_input('Fale com o chat')
+   if prompt:
+      nova_mensagem = {'role':'user', 'content':prompt}
+      chat = st.chat_message(nova_mensagem['role'])
+      chat.markdown(nova_mensagem['content'])
+      mensagens.append(nova_mensagem)
+      
+      chat = st.chat_message('assistant')
+      placeholder = chat.empty()
+      resposta_completa = ''
+
+      placeholder.markdown('| ')
+      respostas = retorna_resposta_modelo_groq(mensagens,
+                                          stream=True)
+      for resposta in respostas:
+          # Verifica se o conteúdo da resposta não é None antes de concatenar
+          if resposta.choices[0].delta.content is not None:
+              resposta_completa += str(resposta.choices[0].delta.content)
+              placeholder.markdown(resposta_completa)  # Atualiza o placeholder com o conteúdo parcial
+      
+      # Cria a nova mensagem apenas se houver conteúdo na resposta completa
+      if resposta_completa:
+          nova_mensagem = {'role': 'assistant', 'content': resposta_completa}
+          mensagens.append(nova_mensagem)
+
+      st.session_state['mensagens'] = mensagens
+
+pagina_principal()
