04e78d526d652d45ece97c8b1e0a7c5e795161e0
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2025-04-30 16:26:05 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,9 +71,9 @@ def pagina_principal():
         chat = st.chat_message(mensagem['role'])
         chat.markdown(mensagem['content'])
 
-    prompt = st.chat_input('Fale com o chat')
+    prompt = st.chat_input('Fale com o chat', accept_file=True,  file_type=["jpg", "jpeg", "png"])
     if prompt:
-        nova_mensagem = {'role': 'user', 'content': prompt}
+        nova_mensagem = {'role': 'user', 'content': prompt['text']}
         chat = st.chat_message(nova_mensagem['role'])
         chat.markdown(nova_mensagem['content'])
         mensagens.append(nova_mensagem)
